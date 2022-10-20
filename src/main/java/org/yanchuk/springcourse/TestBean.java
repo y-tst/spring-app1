fe8047b89ff7c07c5b807f6f5d6e0a26e2bfd0d7
yurii_yanchuk <yurii_yanchuk@epam.com>
2022-10-20 14:32:55 +0300

    commit first bean

--- src/main/java/org/yanchuk/springcourse/TestBean.java
+++ src/main/java/org/yanchuk/springcourse/TestBean.java
@@ -1,2 +1,17 @@
-package org.yanchuk.springcourse;public class TestBean {
+package org.yanchuk.springcourse;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
