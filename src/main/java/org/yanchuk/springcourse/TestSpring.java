2b2b67e66582ebf4192dbece5530d19fab353686
yurii_yanchuk <yurii_yanchuk@epam.com>
2022-10-20 17:45:27 +0300

    add DI

--- src/main/java/org/yanchuk/springcourse/TestSpring.java
+++ src/main/java/org/yanchuk/springcourse/TestSpring.java
@@ -6,10 +6,10 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = context.getBean("musicBean", Music.class);
+//        Music music = context.getBean("musicBean", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
