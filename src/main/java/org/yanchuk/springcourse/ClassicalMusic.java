75bb3d5faf117ab4c967507ff22997ced7f9b634
yurii_yanchuk <yurii_yanchuk@epam.com>
2022-10-26 21:28:04 +0300

    add Init, Destroy, Fabric method

--- src/main/java/org/yanchuk/springcourse/ClassicalMusic.java
+++ src/main/java/org/yanchuk/springcourse/ClassicalMusic.java
@@ -1,6 +1,21 @@
 package org.yanchuk.springcourse;
 
-public class ClassicalMusic implements Music{
+public class ClassicalMusic implements Music {
+    private ClassicalMusic() {
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    public void myInitMethod() {
+        System.out.println("Starting Init method");
+    }
+
+    public void muDestroyMethod() {
+        System.out.println("Starting Destroy Method");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/org/yanchuk/springcourse/TestSpring.java
+++ src/main/java/org/yanchuk/springcourse/TestSpring.java
@@ -6,23 +6,40 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        ClassicalMusic classicalMusic1 = context.getBean("musicBeanForProto", ClassicalMusic.class);
+        ClassicalMusic classicalMusic2 = context.getBean("musicBeanForProto", ClassicalMusic.class);
+
+        System.out.println(classicalMusic.getSong());
+        System.out.println();
+
+        System.out.println(classicalMusic1.getSong());
+        System.out.println(classicalMusic1.getSong());
+        System.out.println(classicalMusic1.getSong());
+        System.out.println();
+
+        System.out.println(classicalMusic2.getSong());
+        System.out.println(classicalMusic2.getSong());
+        System.out.println(classicalMusic2.getSong());
+
+
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
 //        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 //        musicPlayer.playMusic();
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-
-        System.out.println("First player is the same as Second - " + comparison);
-
-        firstMusicPlayer.setVolume(10);
-
-        System.out.println("First MusicPlayer Volume: " + firstMusicPlayer.getVolume());
-        System.out.println("Second MusicPlayer Volume: " + secondMusicPlayer.getVolume());
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//
+//        System.out.println("First player is the same as Second - " + comparison);
+//
+//        firstMusicPlayer.setVolume(10);
+//
+//        System.out.println("First MusicPlayer Volume: " + firstMusicPlayer.getVolume());
+//        System.out.println("Second MusicPlayer Volume: " + secondMusicPlayer.getVolume());
 
 
 //        System.out.println(musicPlayer.getName());
